4948b1aa7877ac44cc2e1ee33c67de6aa3725c24
Kristen <kristen.dynamite@gmail.com>
2017-07-16 20:36:08 -0700

    add mama profiles to admin page

--- src/app/club-list/club-list.component.ts
+++ src/app/club-list/club-list.component.ts
@@ -13,7 +13,7 @@ import { FirebaseListObservable } from 'angularfire2/database';
 
 export class ClubListComponent implements OnInit {
   mamaprofiles: FirebaseListObservable<any[]>;
-
+  currentRoute: string = this.router.url;
 
   constructor(private router: Router, private mamaProfileService: MamaProfileService){}
 
